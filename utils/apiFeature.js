71935ab34734e2694bd99cd22c153e585a615380
risilmrk <risilbinmusthafa@gmail.com>
2025-05-25 06:42:58 +0530

    feat: add pagination to API query handler

--- utils/apiFeature.js
+++ utils/apiFeature.js
@@ -35,6 +35,12 @@ module.exports = class apiFeature {
       }
       return this;
     }
-  
+      paginate() {
+      const page = this.queryString.page * 1 || 1;
+      const limit = this.queryString.limit * 1 || 100;
+      const skip = (page - 1) * limit;
+      this.query = this.query.skip(skip).limit(limit);
+      return this;
+    }
 
   }
